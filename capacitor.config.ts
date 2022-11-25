397def77556cf100e99f7336311557cc5fb558c7
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2022-11-25 10:12:39 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'F1_FantasyTeam',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
