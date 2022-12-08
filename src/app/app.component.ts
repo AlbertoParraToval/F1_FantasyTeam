2f291739ff794f981d1ae442fb5a6751d17f7980
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2022-12-08 19:46:31 +0100

    Traduccion Correcta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,22 @@ import { LocaleService } from './core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  language = 1; // 0 es español, 1 es inglés
   constructor(
     private translate: TranslateService
   ) {
-    this.translate.setDefaultLang("en");
+    this.translate. setDefaultLang('en');
+  }
+  onLanguage(){
+    this.language = (this.language+1)%2;
+    switch(this.language){
+      case 0:
+        this.translate.setDefaultLang('es');
+        break;
+      case 1:
+        this.translate.setDefaultLang('en');
+        break;
+    }
   }
 }
+
