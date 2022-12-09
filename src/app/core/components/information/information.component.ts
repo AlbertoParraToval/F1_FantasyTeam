5400e90796343a3939e1008fe653563e419d5839
AlbertoParraToval <alberto.parra.fp@iescampanillas.com>
2022-12-09 09:11:25 +0100

    Actualizado

--- src/app/core/components/information/information.component.ts
+++ src/app/core/components/information/information.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-information',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./information.component.scss'],
 })
 export class InformationComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {}
